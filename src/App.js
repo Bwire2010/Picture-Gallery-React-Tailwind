9758df01bea6f881869fc2d1737da318a98d8ace
patrobas <patrobasbwire@gmail.com>
2023-06-12 08:39:12 +0300

    created a card

--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-6xl">Hello</h1>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://images.unsplash.com/photo-1497034825429-c343d7c6a68f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGhvdG98ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60" alt="" className="w-full" />
+      <div className="px-6 py-4">
+        <div className="font-bold text-purple-500 text-xl mb-2">
+          photo by John Doe
+        </div>
+      </div>
     </div>
   );
 }
